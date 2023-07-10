7867fd06b449d779d74e02c9d19da1098caabe9a
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-10 23:05:48 +0300

    adds viewset settings for the Course model

---
+++ courses/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from courses.apps import CoursesConfig
+from courses.views import CourseViewSet
+
+app_name = CoursesConfig.name
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='courses')
+
+urlpatterns = [
+] + router.urls
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from courses.models import Course
+from courses.serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
