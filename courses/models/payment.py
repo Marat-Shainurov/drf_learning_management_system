a42bb69ea062b356c6dd47a53bddedbc5a192111
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-13 21:44:43 +0300

    adds filter  to the payment model views

--- courses/models/payment.py
+++ courses/models/payment.py
@@ -7,7 +7,6 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Payment(models.Model):
-
     PAYMENT_TYPE_CHOICE = [
         ('cash', 'Cash'),
         ('transfer', 'Transfer')
@@ -16,9 +15,11 @@ class Payment(models.Model):
     payment_sum = models.PositiveIntegerField(verbose_name='payment_sum')
     payment_date = models.DateTimeField(verbose_name='payment_date', auto_now_add=True)
     payment_type = models.CharField(max_length=10, choices=PAYMENT_TYPE_CHOICE, verbose_name='payment_type')
-    paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='paid_course', **NULLABLE)
-    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='paid_course', **NULLABLE)
-    payment_user = models.ForeignKey(User, verbose_name='payment_user', on_delete=models.CASCADE)
+    paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='paid_course',
+                                    related_name='course', **NULLABLE)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='paid_course',
+                                    related_name='lesson', **NULLABLE)
+    payment_user = models.ForeignKey(User, verbose_name='payment_user', related_name='user', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.payment_user}, {self.paid_lesson if self.paid_lesson else self.paid_course}, {self.payment_sum}'
--- courses/views/payment_views.py
+++ courses/views/payment_views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework import generics
 
 from courses.models import Payment
@@ -11,6 +13,10 @@ class PaymentCreateAPIView(generics.CreateAPIView):
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson',)
+    ordering_fields = ('payment_date',)
+
 
 class PaymentRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = PaymentSerializer
