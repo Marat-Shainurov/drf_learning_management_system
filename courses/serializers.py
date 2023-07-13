b3b3b63a8b919314a5bd877aa0a699059e8fde3f
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-13 16:56:54 +0300

    adds lessons counter to the course detail view, and the course's name to the lesson detail view

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+from rest_framework.relations import SlugRelatedField
 
 from courses.models import Course, Lesson
 
@@ -9,7 +11,28 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CourseDetailSerializer(serializers.ModelSerializer):
+    lessons_count = SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = ('course_title', 'lessons_count')
+
+    def get_lessons_count(self, course):
+        return Lesson.objects.filter(lesson_course=course).count()
+
+
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class LessonDetailSerializer(serializers.ModelSerializer):
+    lesson_course = SlugRelatedField(slug_field='course_title', queryset=Course.objects.all())
+
+    class Meta:
+        model = Lesson
+        fields = (
+        'lesson_title', 'lesson_description', 'lesson_preview', 'link_to_video', 'lesson_course',
+        )
--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, generics
+from rest_framework.response import Response
 
 from courses.models import Course, Lesson
-from courses.serializers import CourseSerializer, LessonSerializer
+from courses.serializers import CourseSerializer, LessonSerializer, LessonDetailSerializer, CourseDetailSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        queryset = Course.objects.all()
+        course = get_object_or_404(queryset, pk=kwargs['pk'])
+        serializer = CourseDetailSerializer(course)
+        return Response(serializer.data)
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
@@ -17,9 +25,11 @@ class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
+
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
-    serializer_class = LessonSerializer
+    serializer_class = LessonDetailSerializer
     queryset = Lesson.objects.all()
+
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
