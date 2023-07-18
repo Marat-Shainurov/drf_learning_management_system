25541e63fa21ee30a1b25b5c9de78b2d28e35c93
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-18 16:04:24 +0300

    adds user field to lesson and course models

--- courses/models/course.py
+++ courses/models/course.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from users.models import NULLABLE
@@ -7,6 +8,8 @@ class Course(models.Model):
     course_title = models.CharField(verbose_name='course_title', max_length=250, unique=True)
     course_preview = models.ImageField(verbose_name='course_preview', upload_to='courses_previews', **NULLABLE)
     course_description = models.TextField(verbose_name='course_description', **NULLABLE)
+    course_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='course_user',
+                                    **NULLABLE)
 
     def __str__(self):
         return f'{self.course_title}'
--- courses/models/lesson.py
+++ courses/models/lesson.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from courses.models import Course
@@ -11,6 +12,8 @@ class Lesson(models.Model):
     link_to_video = models.URLField(verbose_name='link_to_lesson_video', max_length=250, **NULLABLE)
     lesson_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lesson_course',
                                       verbose_name='lesson_course')
-    
+    lesson_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='lesson_user',
+                                    **NULLABLE)
+
     def __str__(self):
         return f'{self.lesson_title}'
