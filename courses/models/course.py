d207d909988c4b7c1035a1fac6e8e619a6c9ad45
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-18 16:20:23 +0300

    updates user fields in lesson and course models

--- courses/models/course.py
+++ courses/models/course.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     course_title = models.CharField(verbose_name='course_title', max_length=250, unique=True)
     course_preview = models.ImageField(verbose_name='course_preview', upload_to='courses_previews', **NULLABLE)
     course_description = models.TextField(verbose_name='course_description', **NULLABLE)
-    course_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='course_user',
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='course_user',
                                     **NULLABLE)
 
     def __str__(self):
