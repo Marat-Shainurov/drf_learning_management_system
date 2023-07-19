4d1dc06b77a004b40a16b00ce92f1568bb2d0283
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-19 20:16:58 +0300

    adds IsOwner and IsModerator permissions to the lesson views

--- courses/views/lesson_views.py
+++ courses/views/lesson_views.py
@@ -2,32 +2,38 @@ from rest_framework import generics
 
 from courses.models import Lesson
 from courses.serializers import LessonSerializer, LessonDetailSerializer
+from courses.permissions import IsOwner, IsModerator
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsModerator]
 
     def perform_create(self, serializer):
         new_lesson = serializer.save()
-        new_lesson.lesson_user = self.request.user
+        new_lesson.user = self.request.user
         new_lesson.save()
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwner]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonDetailSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwner]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwner]
 
 
 class LessonDeleteAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsModerator]
