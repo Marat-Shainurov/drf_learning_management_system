4bbb28e1251a00b6cd55646ddcde16d3cd70cd83
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-19 19:45:26 +0300

    adds IsOwner and IsModerator custom permission classes, adds users

---
+++ courses/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+
+from users.models import UserRoles
+
+
+class IsOwner(BasePermission):
+    message = "You are not the object's owner!"
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.user:
+            return True
+        return False
+
+class IsModerator(BasePermission):
+    message = "You are not a moderator!"
+
+    def has_permission(self, request, view):
+        if request.user.role == UserRoles.MODERATOR:
+            return True
+        return False
+
