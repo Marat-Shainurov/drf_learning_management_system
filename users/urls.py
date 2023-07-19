a2862092939e2943daa5d548715bc4c0126c5fee
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-19 19:48:10 +0300

    adds user create and list views and urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UsersRetrieveAPIView, UsersUpdateAPIView
+from users.views import UsersRetrieveAPIView, UsersUpdateAPIView, UsersCreateAPIView, UsersListAPIView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -14,5 +14,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('detail/<int:pk>/', UsersRetrieveAPIView.as_view(), name='user_detail'),
+    path('create/', UsersCreateAPIView.as_view(), name='user_create'),
+    path('list/', UsersListAPIView.as_view(), name='user_list'),
     path('update/<int:pk>/', UsersUpdateAPIView.as_view(), name='user_update'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,15 @@ from users.models import User
 from users.serializers import UserSerializer
 
 
+class UsersCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UsersListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
 class UsersRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
