df9c0a26cf19091f94ab072b56bf8305c4dcc415
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-21 12:08:03 +0300

    overrides create, list, get_permissions methods for the courses viewset

--- courses/views/course_views.py
+++ courses/views/course_views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from courses.models import Course
+from courses.permissions import IsModerator, IsOwner
 from courses.serializers import CourseSerializer, CourseDetailSerializer
 
 
@@ -13,3 +16,23 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         return self.serializers.get(self.action, self.default_serializer)
+
+    def create(self, request, *args, **kwargs):
+        request.data['user'] = request.user.pk
+        return super().create(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        user = self.request.user
+        queryset = self.filter_queryset(self.get_queryset().filter(user=user))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            permission_classes = [IsAuthenticated, IsModerator]
+        elif self.action in ['retrieve']:
+            permission_classes = [IsAuthenticated, IsOwner | IsModerator]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
