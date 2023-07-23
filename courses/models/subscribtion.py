f051f0afba2b574db059dec7e4e6ae7fb4b0c187
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-23 15:53:25 +0300

    adds the Subscription model

---
+++ courses/models/subscribtion.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+
+from courses.models import Course
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='subscribed_user', on_delete=models.CASCADE,
+                             related_name='subscribed_user')
+    course = models.ForeignKey(Course, verbose_name='subscription_course', on_delete=models.CASCADE,
+                               related_name='subscription_course')
+
+    def __str__(self):
+        return f'{self.user} subscribed to {self.course}'
+
+    class Meta:
+        verbose_name = 'Subscription'
+        verbose_name_plural = 'Subscriptions'
+
+
