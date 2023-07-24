abb151b338aefb91f9c4754dd68567cd045912b0
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-24 17:49:49 +0300

    overrides the perform_create() method (creates a new payment via the stripe package)

--- courses/views/payment_views.py
+++ courses/views/payment_views.py
@@ -1,13 +1,32 @@
+from django.core.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework import generics
 
 from courses.models import Payment
-from courses.serializers import PaymentSerializer
+from courses.serializers import PaymentSerializer, PaymentCreateSerializer
+import stripe
 
 
 class PaymentCreateAPIView(generics.CreateAPIView):
-    serializer_class = PaymentSerializer
+    serializer_class = PaymentCreateSerializer
+
+    def perform_create(self, serializer):
+        new_payment = serializer.save()
+
+        if not new_payment.paid_course and not new_payment.paid_lesson:
+            raise ValidationError("Either paid_course or paid_lesson must be selected.")
+
+        new_payment.user = self.request.user
+        new_payment.payment_sum = new_payment.paid_course.price if new_payment.paid_course else new_payment.paid_lesson.price
+        new_payment.save()
+
+        stripe.api_key = "sk_test_51NXmWXJiDDtWvXOb6yqZ6UDCLLz8kr1wtRVVqMeyDHSL0oIQYlLhUjHDOwKlInBUuUACFQ6zcyO3IhzZTFPfYvM300KGnycyF7"
+        stripe.PaymentIntent.create(
+            amount=new_payment.payment_sum,
+            currency="rub",
+            automatic_payment_methods={"enabled": True},
+        )
 
 
 class PaymentListAPIView(generics.ListAPIView):
