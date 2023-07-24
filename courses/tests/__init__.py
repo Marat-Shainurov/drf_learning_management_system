8a98e21eaf99c51b720e61dc02772d786bee5a27
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-24 03:50:01 +0300

    adds lesson CRUD tests

---
+++ courses/tests/__init__.py
@@ -0,0 +1,4 @@
+from .subscription_tests import SubscriptionCreateTestCase, SubscriptionDeleteTestCase
+from .lesson_tests import LessonCRUDTestCases
+
+__all__ = ['SubscriptionCreateTestCase', 'SubscriptionDeleteTestCase', 'LessonCRUDTestCases']
---
+++ courses/tests/subscription_tests.py
@@ -0,0 +1,9 @@
+from rest_framework.test import APITestCase
+
+
+class SubscriptionCreateTestCase(APITestCase):
+    pass
+
+
+class SubscriptionDeleteTestCase(APITestCase):
+    pass
