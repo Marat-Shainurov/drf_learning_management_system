2cd5df8a8e7ae700b1a5952e67b1a0b42fc0766d
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-24 17:47:30 +0300

    updates the payment model

--- courses/models/payment.py
+++ courses/models/payment.py
@@ -12,14 +12,15 @@ class Payment(models.Model):
         ('transfer', 'Transfer')
     ]
 
-    payment_sum = models.PositiveIntegerField(verbose_name='payment_sum')
+    payment_sum = models.PositiveIntegerField(verbose_name='payment_sum', **NULLABLE)
     payment_date = models.DateTimeField(verbose_name='payment_date', auto_now_add=True)
     payment_type = models.CharField(max_length=10, choices=PAYMENT_TYPE_CHOICE, verbose_name='payment_type')
     paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='paid_course',
                                     related_name='course', **NULLABLE)
-    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='paid_course',
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='paid_lesson',
                                     related_name='lesson', **NULLABLE)
-    payment_user = models.ForeignKey(User, verbose_name='payment_user', related_name='user', on_delete=models.CASCADE)
+    payment_user = models.ForeignKey(User, verbose_name='payment_user', related_name='user', on_delete=models.CASCADE,
+                                     **NULLABLE)
 
     def __str__(self):
         return f'{self.payment_user}, {self.paid_lesson if self.paid_lesson else self.paid_course}, {self.payment_sum}'
