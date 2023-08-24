37462c7ceeb95ae279138a7644b8477b6d929d1a
Marat Shainurov <m_shaynurov@outlook.com>
2023-08-24 09:22:55 +0300

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py migrate
-python3 manage.py collectstatic --input
+python3 manage.py collectstatic --noinput
 deactivate
