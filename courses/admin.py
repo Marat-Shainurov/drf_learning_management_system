5cf7d95ea062ad1c392f3f880b3309f20dd07416
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-21 13:26:03 +0300

    adds Payment and Subscription models to the admin interface, updates it for the Course amd Lesson models

--- courses/admin.py
+++ courses/admin.py
@@ -1,14 +1,33 @@
 from django.contrib import admin
 
-from courses.models import Course, Lesson
+from courses.models import Course, Lesson, Subscription, Payment
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('course_title',)
-    list_filter = ('course_title',)
+    list_display = ('id', 'course_title', 'course_preview', 'user', 'price')
+    list_filter = ('user',)
+    search_fields = ('id', 'course_title')
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_filter = ('lesson_title',)
+    list_display = ('id', 'lesson_title', 'lesson_preview', 'link_to_video', 'lesson_course', 'user', 'price')
+    list_filter = ('user', 'lesson_course',)
+    search_fields = ('id', 'lesson_title')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course',)
+    list_filter = ('course',)
+    search_fields = ('id',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'payment_sum', 'payment_date', 'payment_type', 'paid_course', 'paid_lesson', 'payment_user',
+        'payment_url', 'is_paid', 'payment_id',)
+    list_filter = ('paid_course', 'paid_lesson', 'payment_user', 'is_paid')
+    search_fields = ('id',)
